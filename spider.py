84851435bafa4f0f657e5bb518088652c5d838dd
wslshanlin@sina.com <wslshanlin@sina.com>
2017-10-07 20:20:42 +0800

    fixFeature | get_books_detail |

--- spider.py
+++ spider.py
@@ -6,6 +6,7 @@ import time
 from lxml import etree
 # from lab import *
 from requests import Request, Session
+import requests
 
 
 class Spider:
@@ -14,127 +15,50 @@ class Spider:
         self.url_douban = 'http://book.douban.com'
 
     def get_response(self, url):
-        headers = {
-            'Accept-Charset': 'utf-8',
-        }
-        s = Session()
-        req = Request(url=url, method='get', headers=headers)
-        prepped = req.prepare()
-        resp = s.send(prepped)
-        print(resp.content)
-        # return str(resp.content)
-        return str(resp.content, encoding='gbk').encode('utf8')
+        resp = requests.get(url=url)
+        return resp.text
 
-    # 获取各活动专题链接
     def get_urls_session(self):
-
+        ''' 获取各活动专题链接 '''
         page = self.get_response(self.url_dangdang)
 
         # html源码中寻找字段
         html = etree.HTML(page)
-        print(page)
         result = html.xpath('/html/body/div/div/div/div/div/ul/li/a')
-        print(result)
-        for r in result:
-            print(r)
-            if len(r.xpath('span[1]/text()')) > 0:
-                print(r.xpath('@href')[0])
-                print(r.xpath('@title')[0])
-                print(r.xpath('span[1]/text()')[0].strip())
-        #         todo
-        items = []
-        # pattern = re.compile(r'(<a href="http://v.dangdang.com/pn0_\d*_3.html.*?</span>)', re.S)
-        # r'(<a href="http://v.dangdang.com/pn0_\d*_3.html)', re.S)
+        session_list = [{'session_name': r.xpath('@title')[0] or r.xpath('span[1]/text()')[0].strip(),
+                         'session_url': r.xpath('@href')[0]}
+                        for r in result if len(r.xpath('span[1]/text()')) > 0]
+        return session_list
 
-        # items = re.findall(pattern, page)  # findall(pattern, string, flags=0)
-
-        # 从链接所处字符串位置截取链接
-        sessions = []
-        for item in items:
-
-            sessionurl = item[9:50]
-
-            if len(item.split("\"")) == 7:
-                sessionname = item.split("\"", 7)[6].split("-", 1)[1].split("<", 1)[0]
-
-            elif len(item.split("\"")) == 33:
-                sessionname = item.split("\"", 10)[9].split("-", 1)[1]
-
-            else:
-                print("获取专题名出错")
-
-            session = {'sessionname': sessionname, 'sessionurl': sessionurl}
-            sessions.append(session)
-
-        # 将专题链接地址写入 txt 文件
-        # writeSession_txt(sessions)
-
-        print(sessions)
-        return sessions
-
-    # 获取书籍的书名、购买地址、预览图地址、价格
-    def getBookdetail(self, link='http://v.dangdang.com/pn0_10005415_3.html'):
-
-        # page
-        request = urllib.Request(link)
-        response = urllib.urlopen(request)
-        page = response.read().decode('gbk')
-
-        # pattern
-        pattern = re.compile( \
-            r'(<li.*?</span></span>)', re.S)
-
-        # findall(pattern, page)
-        items = re.findall(pattern, page)  # 查找字段
-        items = items[1:]  # 去掉第一项无用html字段
-
-        # 整理信息
-        books = []
-        for item in items:
-
-            # 剔除售罄书目
-            if re.findall('mix_over', item):
-                items.remove(item)
-                continue
-
-            # 每本书html信息用等号拆分
-            item = item.split("=")
-
-            # 书名中含有等号 = 会导致切分错误，直接跳过
-            if len(item) != 18:
-                continue
-
-            # 将拆分后的每项信息整理
-            book = {}
-            book['booklink'] = item[5].split("\"", 2)[1].encode('utf8')
-            book['bookpic'] = item[7].split("\'", 2)[1].encode('utf8')  # bug
-            book['bookname'] = \
-                item[8].split("\'", 2)[1].split("（", 1)[0].split("(", 1)[0].split("—", 1)[0].split("-", 1)[0].split("：",
-                                                                                                                    1)[
-                    0].encode('utf8')
-            yuan = item[16].split(">", 1)[1].split("<", 1)[0].encode('utf8')
-            jiao = item[17].split(">", 1)[1].split("<", 1)[0].encode('utf8')
-            book['price'] = yuan + jiao
-            books.append(book)
-
-        return books
+    def get_books_detail(self, url_session='http://v.dangdang.com/pn0_10005415_3.html'):
+        ''' 获取书籍的书名、购买地址、预览图地址、价格 '''
+        page = self.get_response(url_session)
+        html = etree.HTML(page)
+        result = html.xpath("/html/body/div/div/div[@class='v_shop_box ']/div[@class='con v_shop_box_list']/ul/li")
+        book_list = [{'book_title': r.xpath('p[1]/a/text()')[0].strip(),
+                      'book_page_url': r.xpath('a/@href')[0],
+                      'book_image': r.xpath('a/img/@src')[0],
+                      'book_price': ''.join([r.xpath('p[2]/span[2]/span[2]/text()')[0].strip(),
+                                             r.xpath('p[2]/span[2]/span[3]/text()')[0].strip()])}
+                     for r in result]
+        return book_list
 
     # 获取书籍的豆瓣评分
-    def getBookrank(self, bookname="无聊的人生，我死也不要"):
-        # books = self.getBookdetail
-
-        bookname = bookname.replace("，", "+").replace(" ", "+").encode('utf8')  #
-
-        url = self.url_douban + "/subject_search?search_text={0}&cat=1001".format(bookname.decode('utf8'))
-
+    def get_books_rank(self, book_title="无聊的人生，我死也不要"):
+        book_title = book_title.replace("，", "+").replace(" ", "+")
+        url = '{0}/subject_search?search_text={1}&cat=1001'.format(self.url_douban, book_title)
+        print(url)
+        # url = self.url_douban + "/subject_search?search_text={0}&cat=1001".format(book_title)
         # 文件头
         user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
         headers = {'User-Agent': user_agent}
-
         # 页面源码
-        request = urllib.Request(url.encode('utf8'))  # somethingwrong
-        response = urllib.urlopen(request)
-        page = response.read()
+        page = self.get_response(url)
+        print(page)
+
+        # request = urllib.Request(url.encode('utf8'))  # somethingwrong
+        # response = urllib.urlopen(request)
+        # page = response.read()
         # print len(page)
 
         pattern = re.compile( \
@@ -154,15 +78,14 @@ class Spider:
             if len(item.split("nums\"".encode('utf8'))) <= 1:
                 continue
 
-            rankurl = item.split("\"", 4)[3].encode('utf8')  # bug
-            bookname = item.split("title=\"", 1)[1].split("\"", 1)[0].encode('utf8')
-            rank = item.split("nums\">", 1)[1].split("<", 1)[0].encode('utf8')
-            rankpeople = item.split("pl\">\n", 1)[1].split("</span>", 1)[0].split("(", 1)[1].split(")", 1)[0].encode(
-                'utf8')
+            rankurl = item.split("\"", 4)[3]  # bug
+            book_title = item.split("title=\"", 1)[1].split("\"", 1)[0]
+            rank = item.split("nums\">", 1)[1].split("<", 1)[0]
+            rankpeople = item.split("pl\">\n", 1)[1].split("</span>", 1)[0].split("(", 1)[1].split(")", 1)[0]
 
             rank = {
                 'rankurl': rankurl,
-                'bookname': bookname,
+                'book_title': book_title,
                 'rank': rank,
                 'rankpeople': rankpeople}
             ranks.append(rank)
@@ -170,89 +93,70 @@ class Spider:
         return ranks
 
     # 完整输出+排序
-    def output():
+    def output(self):
         pass
 
 
 # 查找某专题页书目
-def findBook(details, bookname="中"):
+def find_book(details, book_title="中"):
     books = []
 
     for detail in details:
 
-        if re.findall(bookname.encode('utf8'), detail['bookname'], re.S):
-            print(detail['bookname'], detail['booklink'], detail['price'])
+        if re.findall(book_title.encode('utf8'), detail['book_title'], re.S):
+            print(detail['book_title'], detail['booklink'], detail['price'])
             book = detail
             books.append(book)
 
     return books
 
 
-##############################################################
+def main():
+    spider = Spider()
+
+    print("\n")
+    print("获取专题链接...")
+    slinks = spider.get_urls_session()
+    print("获取到{0}个专题链接".format(len(slinks)))
+
+    datas = []
+    for url in slinks:
+        print("获取专题名...")
+        print("------", url['session_name'], "------")
+
+        print("获取书目...")
+        details = spider.get_books_detail(url['session_url'])
+        print("获取到{0}本书籍".format(len(details)))
+
+        n = 1
+        for detail in details:
+            #
+            #         # writeBook_txt(details)
+            n += 1
+            print("《{0}》".format(detail['book_title']))
+
+            book_title = detail['book_title']  # 这里有问题
+            ranks = spider.get_books_rank(book_title)  # 出错
+            if len(ranks) == 0:
+                print("该书尚未有人评价")
+                continue
+            print("￥" + detail['book_price'], ranks[0]['book_title'], ranks[0]['rank'], ranks[0]['rankpeople'])
+
+            data = {
+                'ddbook_title': detail['book_title'],
+                'ddlink': detail['booklink'],
+                'dbbook_title': ranks[0]['book_title'],
+                'dblink': ranks[0]['rankurl'],
+                'price': float(detail['price']),
+                'rank': float(ranks[0]['rank']),
+                'rankpeople': ranks[0]['rankpeople']}
+            datas.append(data)
+    datas = sorted(datas, key=lambda data: data['price'])
+    datas = sorted(datas, key=lambda data: data['rank'], reverse=True)
+    for data in datas:
+        print(data['rank'])
+        # writeData_txt(datas)
 
 
-spider = Spider()
-
-print("\n")
-print("获取专题链接...")
-slinks = spider.get_urls_session()
-print("获取到{0}个专题链接".format(len(slinks)))
-#
-# datas = []
-# for url in slinks:
-#
-#     print("获取专题名...".encode('utf8'))
-#     print("------", url['sessionname'].encode('utf8'), "------")
-#
-#     print("获取书目...".encode('utf8'))
-#     details = spider.getBookdetail(url['sessionurl'])
-#     print("获取到{0}本书籍".format(len(details)).encode('utf8'))
-#
-#     n = 1
-#     for detail in details:
-#
-#         # writeBook_txt(details)
-#         n += 1
-#         print("《{0}》".format(detail['bookname'].decode('utf8')).encode('utf8'))
-#
-#         bookname = detail['bookname'].decode('utf8')  # 这里有问题
-#         ranks = spider.getBookrank(bookname)  # 出错
-#         if len(ranks) == 0:
-#             print("该书尚未有人评价".encode('utf8'))
-#             continue
-#         print("￥".encode('utf8') + detail['price'], ranks[0]['bookname'], ranks[0]['rank'], ranks[0]['rankpeople'])
-#
-#         data = { \
-#             'ddbookname': detail['bookname'],
-#             'ddlink': detail['booklink'],
-#             'dbbookname': ranks[0]['bookname'],
-#             'dblink': ranks[0]['rankurl'],
-#             'price': float(detail['price']),
-#             'rank': float(ranks[0]['rank']),
-#             'rankpeople': ranks[0]['rankpeople']}
-#         datas.append(data)
-# datas = sorted(datas, key=lambda data: data['price'])
-# datas = sorted(datas, key=lambda data: data['rank'], reverse=True)
-# for data in datas:
-#     print(data['rank'])
-# writeData_txt(datas)
-
-'''
-#print spider.getBookdetail()[0]['bookname']
-'''
-
-# 设置文件头
-'''
-url = 'http://www.server.com/login'  
-
-values = {'username' : 'cqc',  'password' : 'XXXX' } 
-data = urllib.urlencode(values) # urlencode()
-
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' 
-headers = {'User-Agent': user_agent} 
-
-request = urllib.Request(url, data, headers) 
-
-response = urllib.urlopen(request)  
-page = response.read()
-'''
+if __name__ == '__main__':
+    main()
