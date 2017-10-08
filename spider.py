23c532dc7d3e260175677a7a29ea4a57b0823582
wslshanlin@sina.com <wslshanlin@sina.com>
2017-10-08 18:55:52 +0800

    fixFeature |

--- spider.py
+++ spider.py
@@ -15,47 +15,37 @@ class Spider:
     def get_response(self, url):
         headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
         resp = requests.get(url=url, headers=headers)
-
         return resp.text
 
     def get_urls_session(self):
         ''' 获取各活动专题链接 '''
         page = self.get_response(self.url_dangdang)
-
-        # html源码中寻找字段
         html = etree.HTML(page)
         result = html.xpath('/html/body/div/div/div/div/div/ul/li/a')
-
         session_list = [{'session_name': r.xpath('@title')[0] or r.xpath('span[1]/text()')[0].strip(),
                          'session_url': r.xpath('@href')[0]}
                         for r in result if len(r.xpath('span[1]/text()')) > 0]
-
         return session_list
 
     def get_books_detail(self, url_session='http://v.dangdang.com/pn0_10005415_3.html'):
         ''' 获取书籍的书名、购买地址、预览图地址、价格 '''
         page = self.get_response(url_session)
-
         html = etree.HTML(page)
         result = html.xpath("/html/body/div/div/div[@class='v_shop_box ']/div[@class='con v_shop_box_list']/ul/li")
-
         book_list = [{'book_title': r.xpath('p[1]/a/text()')[0].strip(),
                       'book_page_url': r.xpath('a/@href')[0],
                       'book_image': r.xpath('a/img/@src')[0],
                       'book_price': ''.join([r.xpath('p[2]/span[2]/span[2]/text()')[0].strip(),
                                              r.xpath('p[2]/span[2]/span[3]/text()')[0].strip()])}
                      for r in result]
-
         return book_list
 
     def get_books_rank(self, book_title="环界asdfasdf"):
         ''' 获取书籍的豆瓣评分 '''
         book_title = book_title.replace("，", "+").replace(" ", "+")
         url = '{0}/subject_search?search_text={1}&cat=1001'.format(self.url_douban, book_title)
-
         browser = self.browser
         browser.get(url)
-
         book = {}
 
         def book_rank(num, book):
@@ -78,7 +68,6 @@ class Spider:
             pass
         else:
             rank = [book]
-
         return rank
 
 
@@ -90,43 +79,33 @@ def find_book(details, book_title="中"):
             print(detail['book_title'], detail['booklink'], detail['price'])
             book = detail
             books.append(book)
-
     return books
 
 
 def main():
     spider = Spider()
-    # spider.get_books_rank()
-    # return
-
     print("\n")
     print("获取专题链接...")
     slinks = spider.get_urls_session()
     print("获取到{0}个专题链接".format(len(slinks)))
-
     datas = []
     for url in slinks:
         print("获取专题名...")
         print("------", url['session_name'], "------")
-
         print("获取书目...")
         details = spider.get_books_detail(url['session_url'])
         print("获取到{0}本书籍".format(len(details)))
-
         n = 1
         for detail in details:
-            #
-            #         # writeBook_txt(details)
+            # writeBook_txt(details)
             n += 1
             print("《{0}》".format(detail['book_title']))
-
             book_title = detail['book_title']  # 这里有问题
             ranks = spider.get_books_rank(book_title)  # 出错
             if len(ranks) == 0:
                 print("该书尚未有人评价")
                 continue
             print("￥" + detail['book_price'], ranks[0]['book_title'], ranks[0]['rank'], ranks[0]['rank_count'])
-
             data = {
                 'ddbook_title': detail['book_title'],
                 'ddlink': detail['book_page_url'],
